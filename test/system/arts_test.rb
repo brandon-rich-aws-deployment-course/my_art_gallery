ecad1b90a8b33456ec2e7c0c4f64ad47c1d695aa
Brandon Rich <brich@nd.edu>
2024-01-05 23:14:39 -0500

    initial revision

---
+++ test/system/arts_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class ArtsTest < ApplicationSystemTestCase
+  setup do
+    @art = arts(:one)
+  end
+
+  test "visiting the index" do
+    visit arts_url
+    assert_selector "h1", text: "Arts"
+  end
+
+  test "should create art" do
+    visit arts_url
+    click_on "New art"
+
+    fill_in "Artist", with: @art.artist
+    fill_in "Description", with: @art.description
+    fill_in "Image url", with: @art.image_url
+    fill_in "Title", with: @art.title
+    fill_in "Year", with: @art.year
+    click_on "Create Art"
+
+    assert_text "Art was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Art" do
+    visit art_url(@art)
+    click_on "Edit this art", match: :first
+
+    fill_in "Artist", with: @art.artist
+    fill_in "Description", with: @art.description
+    fill_in "Image url", with: @art.image_url
+    fill_in "Title", with: @art.title
+    fill_in "Year", with: @art.year
+    click_on "Update Art"
+
+    assert_text "Art was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Art" do
+    visit art_url(@art)
+    click_on "Destroy this art", match: :first
+
+    assert_text "Art was successfully destroyed"
+  end
+end
