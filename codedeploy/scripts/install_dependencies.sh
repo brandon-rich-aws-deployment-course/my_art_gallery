cda035378f55ea0d8ff9ef26bb5256f7e1aca26d
Brandon Rich <brich@nd.edu>
2024-01-07 20:39:14 -0500

    initial revision

---
+++ codedeploy/scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+sudo amazon-linux-extras install ruby3.2.2 -y
+sudo yum install -y sqlite-devel ruby-devel gcc-c++ gcc libxml2 libxml2-devel libxslt libxslt-devel postgresql-devel
+sudo gem install bundler
+sudo yum update -y
---
+++ codedeploy/user_data.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd ~
+
+echo "Start" >> /tmp/user_data_debug.txt
+echo "$(whoami)" >> /tmp/user_data_debug.txt 
+echo "$(pwd)" >> /tmp/user_data_debug.txt
+
+# patch stuff
+sudo yum update -y
+
+echo "patching done\n" >> /tmp/user_data_debug.txt
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+
+echo "which nvm:" >> /tmp/user_data_debug.txt
+echo "$(which nvm)" >> /tmp/user_data_debug.txt
+
+
+nvm install 20.10.0
+
+echo "checking nvm list" >> /tmp/user_data_debug.txt
+echo "$(nvm list)" >> /tmp/user_data_debug.txt
+
+
+#install yarn
+npm install yarn -g
+
+echo "checking yarn -v" >> /tmp/user_data_debug.txt
+echo "$(yarn -v)" >> /tmp/user_data_debug.txt
+
+
