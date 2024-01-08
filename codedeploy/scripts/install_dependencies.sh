ceb3148c4539f3d6f529f4f1110daf6b45a516fb
Brandon Rich <brich@nd.edu>
2024-01-07 21:30:53 -0500

    updated package dependencies

--- codedeploy/scripts/install_dependencies.sh
+++ codedeploy/scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
-sudo amazon-linux-extras install ruby3.2.2 -y
-sudo yum install -y sqlite-devel ruby-devel gcc-c++ gcc libxml2 libxml2-devel libxslt libxslt-devel postgresql-devel
+# NOTE: Amazon Linux 2023 has a lot more packages available than Amazon Linux 2
+sudo yum install -y sqlite-devel ruby-devel gcc-c++ gcc libxml2 libxml2-devel libxslt libxslt-devel postgresql-devel nodejs libyaml libyaml-devel
 sudo gem install bundler
 sudo yum update -y
