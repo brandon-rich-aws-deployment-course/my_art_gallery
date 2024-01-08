ba32188eb61a3feda35e43261a3b8bf025850780
Brandon Rich <brich@nd.edu>
2024-01-07 21:31:05 -0500

    no longer need node or yarn

--- codedeploy/scripts/start_rails_server.sh
+++ codedeploy/scripts/start_rails_server.sh
@@ -2,25 +2,6 @@
 #. /home/ec2-user/.bashrc
 #export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 
-# install nvm
-echo "DEBUG: Install Node via NVM and install yarn"
-cd ~
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install 14.18.1
-
-#install yarn
-npm install yarn -g
-
-# symlink it so it's in the PATH
-echo "DEBUG: Get yarn in PATH"
-ln -s -f "$(npm bin --global)"/yarn /usr/bin/yarn
-
-
 # move to current deployment folder.  Note: this is a symlink to
 # the latest deploy in /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 cd /opt/current-deployment
