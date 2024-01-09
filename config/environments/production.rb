f1b10c18a9578612231c4822a1a0d5cfb64db813
Brandon Rich <brich@nd.edu>
2024-01-09 17:59:08 -0500

    updated syntax to allow all hostnames in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,5 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   # For demo purposes only -- allows incoming traffic from any domain name.  Real apps can list the DNS values they expect here.
-  config.hosts.clear
+  config.hosts << IPAddr.new("0.0.0.0/0")
 end
