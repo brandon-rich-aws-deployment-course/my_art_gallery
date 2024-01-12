54a19ec1196d2328fd18acde8afa626749ce8717
Brandon Rich <brich@nd.edu>
2024-01-12 09:27:28 -0500

    additional debug info for database

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -5,6 +5,7 @@ class ArtsController < ApplicationController
   def index
     @arts = Art.all
     @hostname = Socket.gethostname
+    @database_adapter = ActiveRecord::Base.connection.adapter_name
   end
 
   # GET /arts/1 or /arts/1.json
