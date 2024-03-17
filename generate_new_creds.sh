0e78bc737d6bf6b39f521b95dc5977f87826490a
Brandon Rich <brich@nd.edu>
2024-03-17 15:03:54 -0400

    fixed bin/rails reference and added line to delete any errant existing credentials files

--- generate_new_creds.sh
+++ generate_new_creds.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Rails is installed, generate credentials and keys
+echo "Deleting any credentials file that may exist..."
+rm config/credentials.yml.enc
 echo "Generating a new master key..."
 bin/rails credentials:edit
 echo "Generating a new secret_key_base..."
 SECRET_KEY_BASE=$(bin/rails secret)
 echo "Re-encrypting credentials with the new key..."
 #echo "$SECRET_KEY_BASE" | bin/rails credentials:edit --force
-EDITOR='echo "secret_key_base=$SECRET_KEY_BASE" >> ' rails credentials:edit
+EDITOR='echo "secret_key_base=$SECRET_KEY_BASE" >> ' bin/rails credentials:edit
