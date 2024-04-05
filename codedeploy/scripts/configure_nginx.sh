5949a5b57ef43b6b7bb9346a5a6d23d0913eaa46
Brandon Rich <brich@nd.edu>
2024-04-05 11:13:30 -0400

    remove all nginx files during preinstall

--- codedeploy/scripts/configure_nginx.sh
+++ codedeploy/scripts/configure_nginx.sh
@@ -1,4 +1,9 @@
 sudo yum install nginx -y
 sudo service nginx stop
-sudo rm /etc/nginx/nginx.conf  # CodeDeploy doesn't support overwrite of pre-existing files.  it only replaces files it created.
+
+# CodeDeploy doesn't support overwrite of pre-existing files.  
+# it only replaces files it created.
+sudo rm /etc/nginx/conf.d/ruby-codedeploy-demo-nginx.conf
+sudo rm /etc/nginx/nginx.conf  
+
 sudo ln -s -f /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive /opt/current-deployment
